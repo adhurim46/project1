dcbf68365456759d2e42e085fbd327086c894ec1
adhurim46 <adhurim.topalli@gmail.com>
2021-07-30 17:32:31 +0200

    aa

--- app/Models/UploadRequests.php
+++ app/Models/UploadRequests.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-class UploadRequest {
+class UploadRequest extends Model {
 
     protected $fillable = ['hash', 'client_ip','filename'];
 
+    public function upload(){
+        return $this->belongsTo(Upload::class);
+    }
    
 }
